13b7989b93f81fc31713ac4e917f41832a8f803c
Gopi Gowda C N <127972364+gopigowda2004@users.noreply.github.com>
2025-11-20 18:15:02 +0530

    Delete fix_emails.sql

--- fix_emails.sql
+++
@@ -1,7 +0,0 @@
-USE FarmTech;
-
-UPDATE user SET email = CONCAT('user', id, '@farmtech.local') WHERE email IS NULL OR email = '';
-
-UPDATE farmers SET email = CONCAT('farmer', id, '@farmtech.local') WHERE email IS NULL OR email = '';
-
-SELECT 'Email update complete' as message;
